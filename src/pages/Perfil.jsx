f1ab7e19b2e93c7392664c3016e5a0ebaf6e0a5e
Ulisses <Ulisses.wrk@gmail.com>
2025-10-14 22:29:00 -0300

    style: altera o texto d a página de perfil

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -2,7 +2,7 @@ export default function Perfil() {
   return (
     <div className="p-6">
       <h2 className="text-2xl font-bold mb-2">Perfil</h2>
-      <p>Essa é a página de perfil (Não entendi o que era pra fazer aqui)</p>
+      <p>Essa é a página de perfil (Não entendi se tinha que ter algo aqui)</p>
     </div>
   );
 }
